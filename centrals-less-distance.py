6ac2e2675c5280fc3b1d45d624d6096e582f11fa
Daniel Kock <daniel.kock@mercadolivre.com>
2020-11-19 10:21:08 -0300

    convert deep search implementation to use class

--- centrals-less-distance.py
+++ centrals-less-distance.py
@@ -1,3 +1,9 @@
+class DeepSearchItem:
+    def __init__(self, vertice):
+        self.vertice = vertice
+        self.visited = False
+        self.distance = float('inf')
+        self.previous = None
 
 
 # monta o grafo 
@@ -17,44 +23,47 @@ def mountGraph(vertices, edges):
 
     return graph
 
+
 # realiza a busca em largura no grafo
 # recebe o grafo e a posicao inicial da busca
-# retorna um array 
+# retorna um array de DeepSearchItem
 def deepSearch(graph, startPosition):
-
     size = len(graph)
+    items = [DeepSearchItem(i) for i in range(size)]
+    
+    items[startPosition].visited = True
+    items[startPosition].distance = 0
 
-    visitedVertices = [False for x in range(size)]
-    distances = [float('inf') for x in range(size)]
-    previousVertices = [None for x in range(size)]
     queue = []
-
-    visitedVertices[startPosition] = True
-    distances[startPosition] = 0
     queue.append(startPosition)
 
     while len(queue) > 0:
+
         c = queue.pop(0)
+
         for i in graph[c]:
-            if(visitedVertices[i] == False):
-                visitedVertices[i] = True
-                distances[i] = distances[c]+1
-                previousVertices[i] = c
+
+            if(items[i].visited == False):
+                items[i].visited = True
+                items[i].distance = items[c].distance + 1
+                items[i].previous = c
                 queue.append(i)
 
-    return distances,previousVertices
+    return items
 
 # monta a rota para sair da origem e chegar no destino
 # recebe a origem, o destino, um array de distancias e um array com a posicao da vertice anterior
 # devolve um array com o menor percurso para o destino a partir da origem
-def mountRoute(source, destination, distances, previousVertices):
+def mountRoute(source, destination, items):
     
     route = []
     route.append(destination)
-    currentPosition = previousVertices[destination]
+    currentPosition = items[destination].previous
     while currentPosition != source and currentPosition != None:
+
         route.append(currentPosition)
-        currentPosition = previousVertices[currentPosition]
+        currentPosition = items[currentPosition].previous
+
     else:
         route.append(currentPosition)
 
@@ -70,7 +79,6 @@ def main():
     c6 = 'c6'
 
     centrals = [c1, c2, c3, c4, c5, c6]
-
     connections = [[c1, c2], [c1, c3], [c1, c4], [c2, c3], [c3, c4], [c3, c6], [c4, c5], [c4, c6], [c5, c6] ]
 
     cs = c1
@@ -82,9 +90,9 @@ def main():
     # monta o grafo
     graph = mountGraph(centrals, connections)
     # realiza a busca em largura com base no elemento cs
-    distances, previousVertices = deepSearch(centrals, graph, csPosition)
+    items = deepSearch(graph, csPosition)
     # monta a rota para chegar em ct a partir de cs
-    route = mountRoute(csPosition, ctPosition, distances, previousVertices)
+    route = mountRoute(csPosition, ctPosition, items)
 
     print(route)
 
